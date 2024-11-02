0abbca0626d8c3fb6c3e60f76772116dbcab1a0e
Kiet Lu <kietlu2202@gmail.com>
2024-11-01 22:36:52 -0600

    Updated the home page.
    Install PrimeNG icon pack. Created a menu bar for experiment. Updated necessary CSS settings.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MenuItem } from 'primeng/api';
+import { MenubarModule } from 'primeng/menubar';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MenubarModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'rental-app';
+export class AppComponent implements OnInit {
+  items: MenuItem[] | undefined;
+
+  ngOnInit(): void {
+    this.items = [
+      { label: 'Home', icon: 'pi pi-home' },
+      { label: 'About Us', icon: 'pi pi-user' },
+      { label: 'Contact', icon: 'pi pi-envelope' },
+    ];
+  }
 }
